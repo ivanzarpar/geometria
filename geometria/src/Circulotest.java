32a9856c3a48d8842159e0c6a21159075ee3972e
ALU1J <ALU1J@PC304>
2020-02-03 11:55:17 +0100

    Realizando test circulo(inacabado)

---
+++ geometria/src/Circulotest.java
@@ -0,0 +1,25 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Circulotest {
+
+	@Test
+	public void testArea() {
+		Circulo_IZP c=new CirculoIZP(7,"Circulo");
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPerimetro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCirculo_IZP() {
+		fail("Not yet implemented");
+	}
+
+}
