aecd5cc379c79beba2493411fa5912c9574b27ba
ALU1J <ALU1J@PC304>
2020-02-03 12:27:09 +0100

    Correción errores en test. AHORA SÍ QUE SÍ

--- geometria/src/Circulotest.java
+++ geometria/src/Circulotest.java
@@ -13,28 +13,28 @@ public class Circulotest {
 	}
 	@Test
 	public void testArea02() {
-		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
+		Circulo_IZP c=new Circulo_IZP(0,"Circulo");
 		assertEquals((0*0*3.1416),c.area(),0.01);
 	}
 	@Test
 	public void testArea03() {
-		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
+		Circulo_IZP c=new Circulo_IZP(-3,"Circulo");
 		assertEquals((-3*-3*3.1416),c.area(),0.01);
 	}
 	@Test
 	public void testPerimetro01() {
 		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
-		assertEquals((7*7*3.1416),c.perimetro(),0.01);;
+		assertEquals((7*2*3.1416),c.perimetro(),0.01);;
 	}
 	@Test
 	public void testPerimetro02() {
-		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
-		assertEquals((0*0*3.1416),c.perimetro(),0.01);;
+		Circulo_IZP c=new Circulo_IZP(0,"Circulo");
+		assertEquals((0*2*3.1416),c.perimetro(),0.01);;
 	}
 	@Test
 	public void testPerimetro03() {
-		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
-		assertEquals((-3*-3*3.1416),c.perimetro(),0.01);;
+		Circulo_IZP c=new Circulo_IZP(-3,"Circulo");
+		assertEquals((-3*2*3.1416),c.perimetro(),0.01);;
 	}
 
 	
