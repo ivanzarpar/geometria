884a12f3e9897dc41ff12770a5e64ae500170827
ALU1J <ALU1J@PC304>
2020-02-03 12:08:18 +0100

    Uff ahora sí

--- geometria/src/Circulotest.java
+++ geometria/src/Circulotest.java
@@ -7,19 +7,36 @@ import org.junit.Test;
 public class Circulotest {
 
 	@Test
-	public void testArea() {
+	public void testArea01() {
 		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
-		fail("Not yet implemented");
+		assertEquals((7*7*3.1416),c.area(),0.01);
+	}
+	@Test
+	public void testArea02() {
+		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
+		assertEquals((0*0*3.1416),c.area(),0.01);
+	}
+	@Test
+	public void testArea03() {
+		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
+		assertEquals((-3*-3*3.1416),c.area(),0.01);
+	}
+	@Test
+	public void testPerimetro01() {
+		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
+		assertEquals((7*7*3.1416),c.perimetro(),0.01);;
+	}
+	@Test
+	public void testPerimetro02() {
+		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
+		assertEquals((0*0*3.1416),c.perimetro(),0.01);;
+	}
+	@Test
+	public void testPerimetro03() {
+		Circulo_IZP c=new Circulo_IZP(7,"Circulo");
+		assertEquals((-3*-3*3.1416),c.perimetro(),0.01);;
 	}
 
-	@Test
-	public void testPerimetro() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCirculo_IZP() {
-		fail("Not yet implemented");
-	}
+	
 
 }
