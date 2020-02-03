75061c5a0065d5e6677c65073c581292c0e64a7e
ALU1J <ALU1J@PC304>
2020-02-03 10:01:41 +0100

    versión refactorizada

--- geometria/src/Circulo_IZP.java
+++ geometria/src/Circulo_IZP.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_IZP extends FiguraGeometrica_IZP {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_IZP(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_IZP.java
+++ geometria/src/FiguraGeometrica_IZP.java
@@ -1,9 +1,9 @@
 //version inicial
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_IZP {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_IZP(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
